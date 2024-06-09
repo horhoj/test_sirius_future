2f66c91fcea58ca9397859deeb8fc3a87b5b18cb
Vasiliy Zadoya <cool29@yandex.ru>
2024-06-09 14:47:08 +0500

    fix HeaderProfile

--- src/layouts/WorkLayout/HeaderProfile/HeaderProfile.tsx
+++ src/layouts/WorkLayout/HeaderProfile/HeaderProfile.tsx
@@ -3,6 +3,7 @@ import styles from './HeaderProfile.module.scss';
 import { HeaderProfileArrowDownIcon, HeaderProfileExitIcon } from '~/assets/icons';
 import { useAppDispatch, useAppSelector } from '~/store/hooks';
 import { authSlice } from '~/features/auth/store/authSlice';
+import { todo } from '~/utils/todo';
 
 export function HeaderProfile() {
   const [open, setIsOpen] = useState(false);
@@ -12,16 +13,39 @@ export function HeaderProfile() {
   };
 
   const fetchUserDataRequest = useAppSelector((state) => state.auth.fetchUserDataRequest);
+  const firstChar = fetchUserDataRequest.data?.name[0] ?? '#';
+
+  const handleOpenToggle = () => {
+    if (fetchUserDataRequest.data === null) {
+      return;
+    }
+    setIsOpen((prev) => !prev);
+  };
 
   return (
     <div className={styles.HeaderProfile}>
-      <button className={styles.arrowDown} onClick={() => setIsOpen((prev) => !prev)}>
+      {firstChar}
+      <button className={styles.arrowDown} onClick={handleOpenToggle}>
         <HeaderProfileArrowDownIcon />
       </button>
       {open && (
         <div className={styles.dropdown}>
           <div className={styles.title}>Смена пользователя</div>
-          <div>{JSON.stringify(fetchUserDataRequest.data, null, 2)}</div>
+          <div className={styles.currentUserWrapper} onClick={todo}>
+            <div className={styles.currentUserAvatar}>{firstChar}</div>
+            <div>
+              <div className={styles.currentUserName}>{fetchUserDataRequest.data?.name}</div>
+              <div>Это вы</div>
+            </div>
+          </div>
+          <div className={styles.currentUserWrapper} onClick={todo}>
+            <div className={styles.currentUserAvatar}>А</div>
+            <div>
+              <div className={styles.currentUserName}>Анна</div>
+              <div>Другой пользователь</div>
+            </div>
+          </div>
+
           <div>
             <button className={styles.exitButton} onClick={handleExit}>
               <span className={styles.exitButtonLabel}>Выход</span>
